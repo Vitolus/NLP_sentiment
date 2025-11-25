039505e56d8a3f3ab64cb56ed88b7829289d28a0
Vitolus <vito.davide00@gmail.com>
2025-11-25 13:46:25 +0100

    add dataset loading

--- src/main.py
+++ src/main.py
@@ -21,16 +21,37 @@ from torchinfo import summary
 from sklearn.model_selection import KFold, train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+from datasets import load_dataset, DatasetDict
 #%%
-seed = 42
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-np.random.seed(seed)
-torch.manual_seed(seed) # if using CPU
-torch.cuda.manual_seed(seed) # if using single-GPU
-torch.cuda.manual_seed_all(seed) # if using multi-GPU
+SEED = 42
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+MODEL_NAME = "distilbert/distilbert-base-uncased"
+DATASET_NAME = "stanfordnlp/imdb"
+np.random.seed(SEED)
+torch.manual_seed(SEED) # if using CPU
+torch.cuda.manual_seed(SEED) # if using single-GPU
+torch.cuda.manual_seed_all(SEED) # if using multi-GPU
 torch.backends.cudnn.deterministic = True # deterministic mode
 torch.backends.cudnn.benchmark = False # disable auto-tuner to find the best algorithm to use for your hardware
 torch.backends.cuda.matmul.allow_tf32 = True # allow TensorFloat-32 on matmul operations
 torch.backends.cudnn.allow_tf32  = True # allow TensorFloat-32 on convolution operations
 torch.autograd.set_detect_anomaly(True)
-print("Using device: ", device)
+print("Using device: ", DEVICE)
+#%% Dataset loading
+dataset = load_dataset(DATASET_NAME)
+print(dataset)
+#%%
+# Just take the first 50 tokens for speed/running on cpu
+def truncate(example):
+    return {
+        'text': " ".join(example['text'].split()[:50]),
+        'label': example['label']
+    }
+
+small_dataset = DatasetDict(
+    train=dataset['train'].shuffle(seed=1111).select(range(128)).map(truncate),
+    val=dataset['train'].shuffle(seed=1111).select(range(128, 160)).map(truncate),
+)
+print(small_dataset)
+print(small_dataset['train'][:10])
