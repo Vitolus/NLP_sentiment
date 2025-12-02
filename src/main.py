8b067ac3f5e0a3b3beb456f24a43a7cefd766cd7
Vitolus <vito.davide00@gmail.com>
2025-12-02 11:49:32 +0100

    Implement model definition, training arguments, and logging callbacks for sentiment analysis

--- src/main.py
+++ src/main.py
@@ -21,7 +21,8 @@ from torchinfo import summary
 from sklearn.model_selection import KFold, train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification, get_linear_schedule_with_warmup, Trainer, TrainingArguments
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification, get_linear_schedule_with_warmup, 
+Trainer, TrainingArguments,TrainerCallback, EarlyStoppingCallback
 from datasets import load_dataset, DatasetDict
 #%%
 SEED = 42
@@ -71,3 +72,60 @@ print(small_tokenized_dataset['train'][0:2])
 #%%
 trainloader = DataLoader(small_tokenized_dataset['train'], batch_size=16, shuffle=True)
 valloader = DataLoader(small_tokenized_dataset['val'], batch_size=16, shuffle=False)
+#%% Model definition
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
+summary(model, input_size=(16, 50), col_names=('input_size', 'output_size', 'num_params', 'trainable'))
+#%%
+def compute_metrics(pred):
+    """Called at the end of validation. Gives accuracy"""
+    logits, labels = pred
+    predictions = np.argmax(logits, axis=-1)
+    # calculates the accuracy
+    return {"accuracy": np.mean(predictions == labels)}
+
+arguments = TrainingArguments(
+    output_dir="./results",
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=1,
+    evaluation_strategy="epoch", # run validation at the end of each epoch
+    save_strategy="epoch",
+    learning_rate=2e-5,
+    load_best_model_at_end=True,
+    seed=SEED
+)
+trainer = Trainer(
+    model=model,
+    args=arguments,
+    train_dataset=small_tokenized_dataset['train'],
+    eval_dataset=small_tokenized_dataset['val'], # change to test when you do your final evaluation!
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+
+class LoggingCallback(TrainerCallback):
+    def __init__(self, log_path):
+        self.log_path = log_path
+    # will call on_log on each logging step, specified by TrainerArguement. (i.e TrainerArguement.logginng_step)
+    def on_log(self, args, state, control, logs=None, **kwargs):
+        _ = logs.pop("total_flos", None)
+        if state.is_local_process_zero:
+            with open(self.log_path, "a") as f:
+                f.write(json.dumps(logs) + "\n")
+
+trainer.add_callback(EarlyStoppingCallback(early_stopping_patience=1, early_stopping_threshold=0.0))
+trainer.add_callback(LoggingCallback("./results/log.jsonl"))
+#%%
+trainer.train()
+#%%
+# just gets evaluation metrics
+# results = trainer.evaluate()
+# also gives you predictions
+results = trainer.predict(small_tokenized_dataset['val'])
+#%%
+# To load our saved model, we can pass the path to the checkpoint into the `from_pretrained` method:
+test_str = "I enjoyed the movie!"
+finetuned_model = AutoModelForSequenceClassification.from_pretrained("./results/checkpoint-???")
+model_inputs = tokenizer(test_str, return_tensors="pt")
+prediction = torch.argmax(finetuned_model(**model_inputs).logits)
+print(["NEGATIVE", "POSITIVE"][prediction])
